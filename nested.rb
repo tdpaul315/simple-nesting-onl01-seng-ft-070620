24b137de8a327673df52037b799a028e691ba2c2
Tiauna Paul <tiaunaj.tsg@gmail.com>
2020-07-16 22:08:16 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,8 +16,8 @@ def hopper
         }
      }
 
-programmer_hash["grace_hopper"][:known_for]
-programmer_hash["grace_hopper"][:languages]
+programmer_hash[:grace_hopper][:known_for]
+programmer_hash[:grace_hopper][:languages]
 
 return programmer_hash
 end
